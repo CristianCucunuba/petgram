fad4cad8e44278c05d7fd318093d9cf9e87cc51e
Cristian Cucunuba <ccucunuba@ipconsultores.net>
2019-07-24 18:13:21 -0500

    Intersection observer with polyfill

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -8,14 +8,20 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [isVisible, setIsVisible] = useState(false)
 
   useEffect(() => {
-    const observer = new window.IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0]
-      if (isIntersecting) {
-        setIsVisible(true)
-        observer.disconnect()
-      }
-    })
-    observer.observe(element.current)
+    Promise.resolve(
+      typeof window.IntersectionObserver !== 'undefined' 
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(()=> {
+        const observer = new window.IntersectionObserver((entries) => {
+          const { isIntersecting } = entries[0]
+          if (isIntersecting) {
+            setIsVisible(true)
+            observer.disconnect()
+          }
+        })
+        observer.observe(element.current)
+      })
   }, [element])
   return (
     <Article ref={element}>
