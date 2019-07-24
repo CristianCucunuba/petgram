65cc1940da01c3bcfcc696baf5fb028b0f5a8d57
Cristian Camilo Cucunubá <cristiancamilo.cucunuba@gmail.com>
2019-07-23 21:04:04 -0500

    Adding Logo and animations

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
-import { GlobalStyles } from './GlobalStyles'
+import { GlobalStyles } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => (
   <Fragment>
     <GlobalStyles />
+    <Logo />
     <ListOfCategories />
     <ListOfPhotoCards />
   </Fragment>
